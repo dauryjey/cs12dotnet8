5d060cd3def00d1909d0a81b8ebc4dcbb46917a0
Daury P. <dauryjey@gmail.com>
2024-04-14 21:40:50 -0400

    Discovering global imports

---
+++ chapter02/Vocabulary/Program.cs
@@ -0,0 +1,9 @@
+﻿//error version
+
+namespace Basics {
+				class Program {
+								static void Main(string[] args) {
+												WriteLine($"Computer named {Env.MachineName} says \"No.\"");
+								}
+				}
+}
