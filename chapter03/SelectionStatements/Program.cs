4aaf866aa4658c77ce23034137310294a21f183a
Daury P. <dauryjey@gmail.com>
2024-04-19 01:16:46 -0400

    Branching with the switch statement

--- chapter03/SelectionStatements/Program.cs
+++ chapter03/SelectionStatements/Program.cs
@@ -2,11 +2,56 @@
 
 string password = "ninja";
 
-if (password.Length < 8) {
+if (password.Length < 8)
+{
 	WriteLine("Your password is too short. Use at least 8 chars.");
 }
 else
 {
 	WriteLine("Your password is long enough.");
 }
+#endregion
+
+#region Pattern matching with the if statement
+
+object o = "3";
+int j = 4;
+
+if (o is int i)
+{
+	WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+	WriteLine("o is not an int so it cannot multiply.");
+}
+
+#endregion
+
+#region Branching with the switch statement
+
+int number = Random.Shared.Next(minValue: 1, maxValue: 7);
+WriteLine($"My random number is {number}");
+switch (number)
+{
+	case 1:
+		WriteLine("One");
+		break; // Jumps to end of switch statement.
+	case 2:
+		WriteLine("Two");
+		goto case 1;
+	case 3: // Multiple case section.
+	case 4:
+		WriteLine("Three or four");
+		goto case 1;
+	case 5:
+		goto A_label;
+	default:
+		WriteLine("Default");
+		break;
+} // End of switch statement.
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
+
 #endregion
